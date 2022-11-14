62c9fd9f52791c42909eadeb413b56392149b3b2
phuc1nguyen <phuc.ng13988@gmail.com>
2022-11-14 11:38:02 +0700

    Add tooltip to RowActions buttons

--- src/components/RowActions/RowActions.tsx
+++ src/components/RowActions/RowActions.tsx
@@ -1,27 +1,31 @@
+import { Tooltip } from '@chakra-ui/react';
 import { FiEdit, FiTrash2, FiEye } from 'react-icons/fi';
 
 type propsType = {
   children?: JSX.Element;
+  title: string;
 };
 
 const RowButton = (props: propsType) => {
-  const { children } = props;
+  const { children, title } = props;
 
   return (
-    <button className="p-3 border border-gray-200 rounded-md hover:bg-blue-50">{children}</button>
+    <Tooltip hasArrow label={title}>
+      <button className="p-3 border border-gray-200 rounded-md hover:bg-blue-50">{children}</button>
+    </Tooltip>
   );
 };
 
 const RowActions = () => {
   return (
     <>
-      <RowButton>
+      <RowButton title="View">
         <FiEye />
       </RowButton>
-      <RowButton>
+      <RowButton title="Edit">
         <FiEdit />
       </RowButton>
-      <RowButton>
+      <RowButton title="Delete">
         <FiTrash2 />
       </RowButton>
     </>
