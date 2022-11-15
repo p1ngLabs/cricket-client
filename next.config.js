aa1d082a41822fda5d05236b1ee59142a119138e
phuc1nguyen <phuc.ng13988@gmail.com>
2022-11-15 17:30:46 +0700

    Create component for displaying user's avatar

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-ipfs.com',
+        pathname: '/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/*.jpg',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
