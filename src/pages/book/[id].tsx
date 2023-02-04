d8ced88fc03c0d29fe61aa46de3fd213cd3d2ede
phuc1nguyen <phuc.ng13988@gmail.com>
2023-02-05 00:46:50 +0700

    Test verification status

---
+++ src/pages/book/[id].tsx
@@ -0,0 +1,11 @@
+import Layout from '@layouts/Layout';
+
+const BookDetails = () => {
+  return (
+    <Layout>
+      <div>book details</div>
+    </Layout>
+  );
+};
+
+export default BookDetails;
